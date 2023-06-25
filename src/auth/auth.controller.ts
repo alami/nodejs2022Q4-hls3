5beb48be0c103347027c55f7087625dc91523e35
aminov <alami@tshtt.uz>
2023-06-25 18:26:58 +0500

    upd auth.controller wt new ValidationPipe()

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Injectable, Post } from "@nestjs/common";
+import {Body, Controller, HttpCode, Post, ValidationPipe} from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import {AuthDto} from "./dto/login.dto";
+import {AuthRefTokenDto} from "./dto/AuthRefTokenDto";
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {}
     @Post('login')
-    loginPlatform(@Body() body: any) {}
+    loginPlatform(@Body(new ValidationPipe()) body: AuthDto) {
+        return this.authService.login(body);
+    }
     @Post('signup')
-    createProfile(@Body() body: any) {}
+    createProfile(@Body(new ValidationPipe()) body: AuthDto) {
+        return this.authService.register(body)
+    }
+    @Post('refresh')
+    @HttpCode(200)
+    refreshToken(@Body(new ValidationPipe()) body: AuthRefTokenDto) {
+        return this.authService.refreshToken(body)
+    }
 }
